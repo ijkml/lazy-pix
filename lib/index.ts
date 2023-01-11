98a6c388d86d36431e246d1fafb9877bb841e606
ML <me.mlaure@gmail.com>
2023-01-11 11:25:53 +0100

    export fn `lazyConfig`

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,2 @@
-export { vuePlugin as vLazyPix } from './vue-plugin';
+export { vuePlugin as vLazyPix, lazyConfig } from './vue-plugin';
 export { useLazyBg, useLazyFg as useLazyImg } from './directives';
